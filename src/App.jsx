eb908e943f7ef37f414c500df97e65799b53d419
Junior Rodrigues <junior.frontend@gmail.com>
2021-06-11 21:24:37 -0300

    starting a new project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './assets/css/base/base.css';
+import Home from './paginas/Home';
+import Sobre from './paginas/Sobre';
+
+function App() {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default App
